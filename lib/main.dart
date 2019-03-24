f7a6b22620bcd81ea9e18d7a6181d5e30c064c4f
Yoga Setiawan <mryoga1995@gmail.com>
2019-03-25 04:29:07 +0700

    Tutorial Flutter #3 - Hallo World (Aplikasi Pertama)

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,17 @@
 import 'package:flutter/material.dart';
-
 void main(){
-  runApp(HomePage());
+  runApp(new MaterialApp(
+    home: new HalHallo(),
+  ));
 }
 
-class HomePage extends StatelessWidget {
-  build(context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.indigo,
-          leading: Icon(Icons.accessible_forward),
-          title: Text("Uji Coba")
-        ),
-        body: Column(
-          children: <Widget>[
-            Image.network("https://www.petanikode.com/img/flutter/flutter.png"),
-            Text("Belajar Flutter",style: TextStyle(fontFamily: "Serif", fontSize: 20, height: 2.0),textAlign: TextAlign.right,),
-            Text("By Arioki Dev",textAlign: TextAlign.left),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text("1"),
-                Text("2"),
-                Text("3"),
-                Text("4"),
-                Text("5"),
-                Text("6"),
-              ],
-            )
-          ],
-        )
+class HalHallo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      body: new Center(
+        child: new Text("Hello Word"),
       ),
     );
   }
