6e18d73d55ee9f2da067391076f963d1558c4c7d
Yoga Setiawan <mryoga1995@gmail.com>
2019-03-25 04:49:49 +0700

    Tutorial Flutter #4 - Membuat Style, Text dan Icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
 void main(){
   runApp(new MaterialApp(
-    home: new HalHallo(),
+    home: new HalamanSatu(),
   ));
 }
 
-class HalHallo extends StatelessWidget {
+class HalamanSatu extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-      body: new Center(
-        child: new Text("Hello Word"),
+      body:
+      new Center(
+        child: new Container(
+        color: Colors.blue,
+        width: 200.0,
+        height: 100.0,
+        child: new Center(
+          child: new Icon(Icons.access_time,color:Colors.white,size:70)
+        ),
       ),
+    ),
     );
   }
-
 }
