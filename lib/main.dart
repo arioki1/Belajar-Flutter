53c75ae991525c86a37b4cf008cce25b322f599d
Yoga Setiawan <mryoga1995@gmail.com>
2019-03-25 08:19:08 +0700

    Tutorial Flutter #7 - Card & Parsing Data

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(new MaterialApp(
-    title: "Tutorial Flutter 5",
+    title: "Card & Parsing Data",
     home: new HalamanSatu(),
   ));
 }
@@ -17,33 +17,48 @@ class HalamanSatu extends StatelessWidget {
           leading: new Icon(Icons.home),
           backgroundColor: Colors.blue,
           title: new Center(
-            child: new Text("Tutorial Flutter 5"),
+            child: new Text("Card & Parsing Data"),
           ),
           actions: <Widget>[new Icon(Icons.search)],
         ),
         body: new Container(
-            child: (
-                new Column(
-                  children: <Widget>[
-                    new Icon(Icons.account_balance, size: 70,),
-                    new Icon(Icons.account_balance, size: 70,),
-                    new Icon(Icons.account_balance, size: 70,),
-                    new Row(children: <Widget>[
-                      new Icon(Icons.account_balance, size: 70,),
-                      new Icon(Icons.account_balance, size: 70,),
-                      new Icon(Icons.account_balance, size: 70,),
-                      new Icon(Icons.account_balance, size: 70,),
-                      new Icon(Icons.account_balance, size: 70,),
-                    ],
-                    ),
-                    new Icon(Icons.account_balance, size: 70,),
-                    new Icon(Icons.account_balance, size: 70,),
-                    new Icon(Icons.account_balance, size: 70,),
-                    new Icon(Icons.account_balance, size: 70,),
-                  ],
-                )
-            )
-        )
-    );
+            child: (new Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                new CardSaya(icon: Icons.ac_unit, text: "Unit"),
+                new CardSaya(icon: Icons.account_balance, text: "balance"),
+                new CardSaya(icon: Icons.access_alarm, text: "alaram"),
+              ],
+            ))));
+  }
+}
+
+class CardSaya extends StatelessWidget {
+
+  final IconData icon;
+  final String text;
+
+  CardSaya({
+    this.icon, this.text
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Container(
+      padding: new EdgeInsets.all(10),
+        child: new Card(
+          child: new Column(
+            children: <Widget>[
+              new Icon(
+                icon,
+                size: 70,
+                color: Colors.black,
+              ),
+              new Text(text, style: new TextStyle(fontSize: 20))
+            ],
+          ),
+        ));
   }
 }
