08e2db8a74c6f024d8bb2a47ed14ebd43ba68416
Yoga Setiawan <mryoga1995@gmail.com>
2019-03-25 07:42:05 +0700

    Tutorial Flutter #6 - Mengatur Layout (Row dan Column)

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,49 @@
 import 'package:flutter/material.dart';
-void main(){
+
+void main() {
   runApp(new MaterialApp(
     title: "Tutorial Flutter 5",
     home: new HalamanSatu(),
   ));
 }
 
-class HalamanSatu extends StatelessWidget{
+class HalamanSatu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-      backgroundColor: Colors.yellow,
-      appBar: new AppBar(
-        leading: new Icon(Icons.home),
-        backgroundColor: Colors.deepOrange,
-        title: new Center(
-          child: new Text("Tutorial Flutter 5"),
+        backgroundColor: Colors.white,
+        appBar: new AppBar(
+          leading: new Icon(Icons.home),
+          backgroundColor: Colors.blue,
+          title: new Center(
+            child: new Text("Tutorial Flutter 5"),
+          ),
+          actions: <Widget>[new Icon(Icons.search)],
         ),
-        actions: <Widget>[
-          new Icon(Icons.search)
-        ],
-      ),
+        body: new Container(
+            child: (
+                new Column(
+                  children: <Widget>[
+                    new Icon(Icons.account_balance, size: 70,),
+                    new Icon(Icons.account_balance, size: 70,),
+                    new Icon(Icons.account_balance, size: 70,),
+                    new Row(children: <Widget>[
+                      new Icon(Icons.account_balance, size: 70,),
+                      new Icon(Icons.account_balance, size: 70,),
+                      new Icon(Icons.account_balance, size: 70,),
+                      new Icon(Icons.account_balance, size: 70,),
+                      new Icon(Icons.account_balance, size: 70,),
+                    ],
+                    ),
+                    new Icon(Icons.account_balance, size: 70,),
+                    new Icon(Icons.account_balance, size: 70,),
+                    new Icon(Icons.account_balance, size: 70,),
+                    new Icon(Icons.account_balance, size: 70,),
+                  ],
+                )
+            )
+        )
     );
   }
 }
