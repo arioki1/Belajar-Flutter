1e29025125c0b9994c76e02a76536ef5efa4f21b
Yoga Setiawan <mryoga1995@gmail.com>
2019-04-03 19:14:20 +0700

    git ingrone

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(new MaterialApp(
-    title: "Tutorial Flutter 5",
-    home: new HalamanSatu(),
-    routes: <String, WidgetBuilder>{
-      '/HalSatu' : (BuildContext context) => HalamanSatu(),
-      '/HalDua' : (BuildContext context) => HalamanDua(),
-    },
-  ));
+void main(){
+  runApp(
+      new MaterialApp(
+        title: "Flutter App",
+        home: new Home(),
+      )
+  );
 }
 
-class HalamanSatu extends StatelessWidget {
+
+class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return new Scaffold(
-      appBar: new AppBar(title: new Text("Halaman Satu"),),
-      body: new Center(
-        child: new IconButton(
-            icon: new Icon(Icons.account_balance, size: 50,),
-            onPressed: (){
-              Navigator.pushNamed(context, '/HalDua');
-            }
-        ),
-      ),
-    );
+    return null;
   }
-}
 
-class HalamanDua extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return new Scaffold(
-      appBar: new AppBar(title: new Text("Halaman Dua"),),
-      body: new Center(
-        child: new IconButton(
-            icon: new Icon(Icons.account_balance, size: 50,),
-            onPressed:(){
-              Navigator.pushNamed(context, '/HalSatu');
-            }
-        ),
-      ),
-    );
-  }
 }
